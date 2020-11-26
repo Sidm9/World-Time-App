4f4db49c3fe810f5f0088cc3ecbb781878be191b
Sidm9 <siddharthm1999@outlook.com>
2020-11-26 09:27:52 +0530

    Added Flags

--- lib/Pages/choose_location.dart
+++ lib/Pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:worldtime/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -8,27 +9,46 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   int counter = 0;
 
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     // print("build function run!!!");
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.blue[900],
         title: Text('Choose A Location'),
         centerTitle: true,
       ),
-      body: RaisedButton(
-        onPressed: () => {
-          setState(() {
-            counter += 1;
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    print(locations[index].location);
+                  },
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        AssetImage('asset/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
           }),
-        },
-        child: Text(
-          'Counter is $counter',
-          style: TextStyle(color: Colors.grey, fontSize: 25),
-        ),
-      ),
     );
   }
 }
